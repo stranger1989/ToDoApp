cfb1414463b7efcc034bea460379035365fd0d52
kuroda-kentaro <kuroda_business@outlook.jp>
2018-12-30 17:19:31 +0900

    Add_sign

--- ToDoApp/SecondController.swift
+++ ToDoApp/SecondController.swift
@@ -19,10 +19,8 @@ class SecondController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
